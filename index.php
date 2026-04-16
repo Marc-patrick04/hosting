70fe7ad6b344a14d82017023277e00506fb0afc4
Marc <marcpatrick04@gmail.com>
2026-04-16 16:55:06 +0200

    corrected

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $host = 'ep-shy-fire-andfpc8g-pooler.c-6.us-east-1.aws.neon.tech';
 $dbname = 'neondb';
 $username = 'neondb_owner';
-$password = 'npg_6jMJEIG3BWeop';
+$password = 'npg_6jMJEIG3BWeo';
 $port = '5432'; 
 
 try {
